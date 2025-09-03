9b55ec61ed56a276a6cd1fc6656856e02ada6152
Jessica Vitoriano <jessica.svitoriano@gmail.com>
2025-09-03 15:19:32 -0300

    update: criado rota de login

--- Program.cs
+++ Program.cs
@@ -3,4 +3,19 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapPost("/login", (loginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com.br" && loginDTO.Password == "123456")
+    {
+        return Results.Ok("Login com sucesso");
+    }
+
+    return Results.Unauthorized();
+});
+
 app.Run();
+public class loginDTO
+{
+    public string Email { get; set; } = default!;
+    public string Password { get; set; } = default!;
+}
